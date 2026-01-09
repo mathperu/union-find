a8d2fe52394f7c500b41efa888ffd9a2bbbb2630
Julian Marmier <julianmarmier@gmail.com>
2026-01-09 11:43:55 +0100

    Revert "going nowhere"
    This reverts commit 3f96aa5ce94ec153920f2df0eadad4b534fce8d6.

--- src/UnionFindSpecs.scala
+++ src/UnionFindSpecs.scala
@@ -57,29 +57,7 @@ object UnionFindSpecs {
       uf.union(a1, a2)._2 == uf.find(a1) || uf.union(a1, a2)._2 == uf.find(a2)
     )
 
-    def updatedListPreservesFind(
-        rootAddr: BigInt,
-        n: Node[T],
-        b: BigInt
-    ): Unit = {
-      require(uf.isValidAddr(rootAddr))
-      require(uf.isValidAddr(b))
-      require(uf.find(b) == rootAddr)
-
-      // val updatedUF = UF(uf.heap.updated(addr, n))
-
-      // if uf.find(b) == addr then assert(updatedUF.find(b) == n.parent)
-      // else assert(updatedUF.find(b) == uf.find(b))
-    }.ensuring(_ => {
-      val updatedUF = UF(uf.heap.updated(rootAddr, n))
-      updatedUF.find(b) == rootAddr
-    })
-
-    def linkPreservesOneRoot(
-        a1: BigInt,
-        a2: BigInt,
-        b: BigInt
-    ): Unit = {
+    def linkMergedTheSets(a1: BigInt, a2: BigInt, b: BigInt): Unit = {
       require(uf.isValidAddr(a1))
       require(uf.isValidAddr(a2))
       require(uf.isValidAddr(b))
@@ -87,56 +65,13 @@ object UnionFindSpecs {
       require(uf.nodeAtIsRoot(a1))
       require(uf.nodeAtIsRoot(a2))
 
-      require(uf.find(b) == a1 || uf.find(b) == a2)
-
-      val (newUF, newRoot) = uf.link(a1, a2)
-
-      (uf.nodeAt(a1), uf.nodeAt(a2)) match {
-        case (n1, n2) =>
-          if n1.rank < n2.rank then
-            // OurListSpecs.mapAtIndex(newUF.heap, a1, _.parent == a2)
-            // OurListSpecs.mapAtIndex(newUF.heap, a2, _.rank == n2.rank)
-            assert(newRoot == a2)
-            // assert(newUF.heap.contains(uf.nodeAt(b)))
-            updatedListPreservesFind(a2, newUF.nodeAt(a2), b)
-            assert(newUF.find(b) == a2)
-          else if n1.rank > n2.rank then
-            // OurListSpecs.mapAtIndex(newUF.heap, a2, _.parent == a1)
-            // OurListSpecs.mapAtIndex(newUF.heap, a1, _.rank == n1.rank)
-            assert(newRoot == a1)
-            assert(newUF.heap.contains(uf.nodeAt(b)))
-            updatedListPreservesFind(a1, newUF.nodeAt(a1), b)
-            assert(newUF.find(b) == a1)
-          else
-            // OurListSpecs.mapAtIndex(newUF.heap, a2, _.parent == a1)
-            // OurListSpecs.mapAtIndex(newUF.heap, a1, _.rank == n1.rank + 1)
-            assert(newRoot == a1)
-            assert(newUF.heap.contains(uf.nodeAt(b)))
-            updatedListPreservesFind(a1, newUF.nodeAt(a1), b)
-            assert(newUF.find(b) == a1)
-      }
-
     }.ensuring(_ => {
       val (newUF, newRoot) = uf.link(a1, a2)
-      newUF.find(b) == a1 || newUF.find(b) == a2
-    })
 
-    def linkNeitherRoot(
-        a1: BigInt,
-        a2: BigInt,
-        b: BigInt
-    ): Unit = {
-      require(uf.isValidAddr(a1))
-      require(uf.isValidAddr(a2))
-      require(uf.isValidAddr(b))
-
-      require(uf.nodeAtIsRoot(a1))
-      require(uf.nodeAtIsRoot(a2))
-
-      require(uf.find(b) != a1 && uf.find(b) != a2)
-    }.ensuring(_ => {
-      val (newUF, newRoot) = uf.link(a1, a2)
-      newUF.find(b) != a1 && newUF.find(b) != a2
+      (!(uf.equiv(a1, b) || uf.equiv(a2, b)) || newUF
+        .find(b) == newRoot)
+      && ((uf.equiv(a1, b) || uf.equiv(a2, b)) || newUF
+        .find(b) != newRoot)
     })
 
     // if b is represented by either a1 or a2 then after union b it is represented by their union
@@ -145,21 +80,13 @@ object UnionFindSpecs {
       require(uf.isValidAddr(a2))
       require(uf.isValidAddr(b))
 
-      val r1 = uf.find(a1)
-      val r2 = uf.find(a2)
-      val (newUF, newRoot) = uf.union(a1, a2)
-
+      linkMergedTheSets(uf.find(a1), uf.find(a2), b)
       if uf.equiv(a1, b) || uf.equiv(a2, b) then
-        // assert(uf.nodeAtIsRoot(r1) || uf.nodeAtIsRoot(r2))
-        // check(uf.find(b) == r1 || uf.find(b) == r2)
-        linkPreservesOneRoot(r1, r2, b)
-        // check(newRoot == r1 || newRoot == r2)
-        // check(newUF.find(b) == r1 || newUF.find(b) == r2)
+        val (newUF, newRoot) = uf.union(a1, a2)
+        assert(newUF.find(b) == newRoot)
       else
-        // check(!uf.equiv(a1, b) && !uf.equiv(a2, b))
-        // check(uf.find(b) != r1 && uf.find(b) != r2)
-        linkNeitherRoot(r1, r2, b)
-        // assert(newUF.find(b) != newRoot)
+        val (newUF, newRoot) = uf.union(a1, a2)
+        assert(newUF.find(b) != newRoot)
 
       // a1 and a2 in same set => their roots are the same
     }.ensuring(_ => {
